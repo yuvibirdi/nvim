2882d70c7d2d59bc6b571c0d04ad05c6a5904d07
ystdin <yuvibirdi27@gmail.com>
2025-11-14 11:28:12 -0500

    comit workings just doing minor dbg fixing

--- lua/plugins/compile.lua
+++ lua/plugins/compile.lua
@@ -119,7 +119,7 @@ return {
 
         local full_cmd
         if run_cmd then
-          full_cmd = string.format('cd %s && %s && echo "\n--- Running ---" && %s', shell_dir, compile_cmd, run_cmd)
+          full_cmd = string.format('cd %s && %s && echo "" && %s && echo ""', shell_dir, compile_cmd, run_cmd)
         else
           full_cmd = string.format('cd %s && %s', shell_dir, compile_cmd)
         end
