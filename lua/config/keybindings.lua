9a1f194d4ef87bf00e72ab73b26bc853cd3034b6
ystdin <yuvibirdi27@gmail.com>
2025-11-14 10:28:47 -0500

    pushing minimal config

--- lua/config/keybindings.lua
+++ lua/config/keybindings.lua
@@ -8,21 +8,7 @@ local function map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, options)
 end
 
--- # Requirement 2: IBuffer-like system
--- <leader>bi -> "buffer index" (shows the list)
-map("n", "<leader>bi", function()
-  require("telescope.builtin").buffers()
-end, { desc = "Find Buffer (IBuffer)" })
-
--- <leader>bn -> "buffer next"
+-- Quick buffer controls without extra plugins
 map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Buffer Next" })
-
--- <leader>bp -> "buffer previous"
 map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Buffer Previous" })
-
-
--- # Bonus: Project Switching
--- A map for your new project.nvim plugin
-map("n", "<leader>fp", function()
-  require("telescope").extensions.projects.projects()
-end, { desc = "Find Projects" })
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,18 +1,1 @@
-return {
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-      -- your configuration comes here
-    },
-    keys = {
-      {
-        "<leader>?",
-        function()
-          require("which-key").show({ global = false })
-        end,
-        desc = "Buffer Local Keymaps (which-key)",
-      },
-    },
-  }
-}
+return {}
