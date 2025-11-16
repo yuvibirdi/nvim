71a92d4f75918351debbdb93d82144362eb5fe7f
ystdin <yuvibirdi27@gmail.com>
2025-11-15 20:13:13 -0500

    trying another fix to just get headers working

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,8 +26,10 @@ return {
         -- Fallback flags for Ubuntu - explicitly include the g++ headers
         init_opts.fallbackFlags = {
           "-std=c++17",
-          "-stdlib=libstdc++",
-          "-I/usr/include/x86_64-linux-gnu/c++/13/",
+          "-I/usr/include/c++/13",
+          "-I/usr/include/x86_64-linux-gnu/c++/13", 
+          "-I/usr/include",
+          "-I/usr/include/x86_64-linux-gnu",
         }
       elseif is_mac then
         local xcode_sdk = vim.fn.trim(vim.fn.system("xcrun --show-sdk-path"))
