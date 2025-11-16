4d88549f732f643130c75652899363d85a6cd541
ystdin <yuvibirdi27@gmail.com>
2025-11-15 20:26:31 -0500

    added more include

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,7 @@ return {
           "-std=c++17",
           "-stdlib=libstdc++",
           "-I/usr/include/x86_64-linux-gnu/c++/13",
+          "-I/usr/include/c++/13"
         }
       elseif is_mac then
         local xcode_sdk = vim.fn.trim(vim.fn.system("xcrun --show-sdk-path"))
