b2b50883aa095073d85959741d6abd5ba0d6e239
yuvibirdi <yuvibirdi27@gmail.com>
2026-01-21 11:33:45 -0500

    bug fixes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,16 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
 
+-- Auto-cd to the directory of the current file
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    if bufname ~= "" and vim.fn.isdirectory(bufname) == 0 then
+      local dir = vim.fn.fnamemodify(bufname, ":p:h")
+      if vim.fn.isdirectory(dir) == 1 then
+        vim.cmd.cd(dir)
+      end
+    end
+  end,
+})
+
